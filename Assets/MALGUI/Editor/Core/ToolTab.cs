27e59276d5dd54baa6fa027c3f557d96c2a763f5
Carlijavier5 <carlijavier5@gmail.com>
2023-09-01 15:48:52 -0400

    Namespace Update! And a bit more. On my way to revamp the Hierarchy Builder!

---
+++ Assets/MALGUI/Editor/Core/ToolTab.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+namespace ModelAssetDatabase {
+
+    /// <summary>
+    /// A base class for all database tool tabs;
+    /// </summary>
+    public abstract class ToolTab : ScriptableObject {
+
+        /// <summary> Every tab will be managed by a parent tool, and will have a handy reference to it; </summary>
+        protected BaseTool Tool;
+
+        protected const string INVALID_MANAGER = "You attempted to create a new tab without a proper manager to handle it! The tab was not instantiated;";
+
+        /// <summary>
+        /// Initialize base tab data when constructing the tab;
+        /// </summary>
+        public static T CreateTab<T>(BaseTool tool) where T : ToolTab {
+            var tab = CreateInstance<T>();
+            tab.Tool = tool;
+            tab.InitializeData();
+            return tab;
+        }
+
+        /// <summary>
+        /// Override this method to implement a custom initialization when the tab is created;
+        /// </summary>
+        protected abstract void InitializeData();
+
+        /// <summary>
+        /// Load the corresponding data, if any, when selecting the tab;
+        /// </summary>
+        public virtual void LoadData(string path) { }
+
+        /// <summary>
+        /// Reset tab dependent data when abandoning the tab;
+        /// </summary>
+        public virtual void ResetData() { }
+
+        void OnDisable() => ResetData();
+
+        public abstract void ShowGUI();
+    }
+
+    public abstract class ReaderTab : ToolTab {
+
+        protected Reader Reader;
+
+        protected override void InitializeData() {
+            if (Tool is Reader) {
+                Reader = Tool as Reader;
+            } else Debug.LogError(INVALID_MANAGER);
+        }
+    }
+
+    public abstract class HierarchyTab : ToolTab {
+
+        protected HierarchyBuilder HierarchyBuilder;
+
+        protected override void InitializeData() {
+            if (Tool is HierarchyBuilder) {
+                HierarchyBuilder = Tool as HierarchyBuilder;
+            } else Debug.LogError(INVALID_MANAGER);
+        }
+    }
+    /*
+    public class HierarchyTabModels : HierarchyTab {
+
+    }
+
+    public class HierarchyTabFolders : HierarchyTab {
+
+    }
+
+    public class HierarchyTabMaterials : HierarchyTab {
+
+    }*/
+}
--- Assets/MALGUI/Editor/GUI/ModelAssetDatabaseGUI.cs
+++ Assets/MALGUI/Editor/GUI/ModelAssetDatabaseGUI.cs
@@ -2,191 +2,199 @@ using UnityEngine;
 using UnityEditor;
 using CJUtils;
 
-/// <summary> Main GUI of the Model Asset Library;
-/// <br></br> Connects a number of tools together in a single window;
-/// </summary>
-public class ModelAssetDatabaseGUI : EditorWindow {
-
-    /// <summary>
-    /// Shows the Main Window of the Model Asset Library;
+namespace ModelAssetDatabase {
+    
+    /// <summary> Main GUI of the Model Asset Library;
+    /// <br></br> Connects a number of tools together in a single window;
     /// </summary>
-    [MenuItem("Tools/Model Asset Library")]
-    public static void ShowWindow() {
-        if (HasOpenInstances<ModelAssetDatabaseGUI>()) MainGUI.Close();
-        ModelAssetLibraryConfigurationCore.LoadConfig();
-        if (string.IsNullOrWhiteSpace(ModelAssetLibrary.RootAssetPath)) {
-            ModelAssetLibraryConfigurationGUI.ShowWindow();
-            return;
-        } MainGUI = GetWindow<ModelAssetDatabaseGUI>("Model Asset Library", typeof(ModelAssetLibraryConfigurationGUI));
-        if (HasOpenInstances<ModelAssetLibraryConfigurationGUI>()) {
-            ModelAssetLibraryConfigurationGUI.ConfigGUI.Close();
+    public class ModelAssetDatabaseGUI : EditorWindow {
+
+        /// <summary>
+        /// Shows the Main Window of the Model Asset Library;
+        /// </summary>
+        [MenuItem("Tools/Model Asset Library")]
+        public static void ShowWindow() {
+            if (HasOpenInstances<ModelAssetDatabaseGUI>()) MainGUI.Close();
+            ConfigurationCore.LoadConfig();
+            if (string.IsNullOrWhiteSpace(ModelAssetDatabase.RootAssetPath)) {
+                ConfigurationGUI.ShowWindow();
+                return;
+            } MainGUI = GetWindow<ModelAssetDatabaseGUI>("Model Asset Library", typeof(ConfigurationGUI));
+            if (HasOpenInstances<ConfigurationGUI>()) {
+                ConfigurationGUI.ConfigGUI.Close();
+            }
         }
-    }
 
-    #region | LibraryGUI-only variables |
+        #region | LibraryGUI-only variables |
 
-    /// <summary> Reference to the active GUI Window; </summary>
-    public static ModelAssetDatabaseGUI MainGUI { get; private set; }
+        /// <summary> Reference to the active GUI Window; </summary>
+        public static ModelAssetDatabaseGUI MainGUI { get; private set; }
 
-    private ModelAssetLibraryHierarchyBuilder hierarchyBuilder;
-    public ModelAssetDatabaseTool[] Subtools { get; private set; }
+        private HierarchyBuilder hierarchyBuilder;
+        public BaseTool[] Subtools { get; private set; }
 
-    /// <summary> Available tools to display in the library; </summary>
-    public enum ToolMode {
-        ModelReader = 0,
-        PrefabOrganizer = 1,
-        MaterialManager = 2,
-    } /// <summary> The tool actively displayed within the library; </summary>
-    private ToolMode toolMode;
+        /// <summary> Available tools to display in the library; </summary>
+        public enum ToolMode {
+            ModelReader = 0,
+            PrefabOrganizer = 1,
+            MaterialManager = 2,
+        } /// <summary> The tool actively displayed within the library; </summary>
+        public ToolMode ActiveTool { get; private set; }
 
-    /// <summary> Whether the window should repaint every frame; </summary>
-    private bool highRepaintFrequency;
+        /// <summary> Whether the window should repaint every frame; </summary>
+        private bool highRepaintFrequency;
 
-    private Vector2 directoryScroll;
-    private Vector2 toolScroll;
+        private Vector2 directoryScroll;
+        private Vector2 toolScroll;
 
-    #endregion
+        #endregion
 
-    #region | Hierarchy-Tool Bridge |
+        #region | Hierarchy-Tool Bridge |
 
-    /// Though in separate classes, the hierarchy builder and the standard tools must occasionally communicate with one another;
-    /// Since references to these instances are stored in the Main GUI, communication between the two happens here;
+        /// Though in separate classes, the hierarchy builder and the standard tools must occasionally communicate with one another;
+        /// Since references to these instances are stored in the Main GUI, communication between the two happens here;
 
-    public ModelAssetDatabaseReader ModelReader { get { return Subtools[(int) ToolMode.ModelReader] as ModelAssetDatabaseReader; } }
-    public ModelAssetDatabasePrefabOrganizer PrefabOrganizer { get { return Subtools[(int) ToolMode.PrefabOrganizer] as ModelAssetDatabasePrefabOrganizer; } }
-    public ModelAssetDatabaseMaterialManager MaterialManager { get { return Subtools[(int) ToolMode.MaterialManager] as ModelAssetDatabaseMaterialManager; } }
+        public Reader ModelReader { get { return Subtools[(int) ToolMode.ModelReader] as Reader; } }
+        public PrefabOrganizer PrefabOrganizer { get { return Subtools[(int) ToolMode.PrefabOrganizer] as PrefabOrganizer; } }
+        public MaterialManager MaterialManager { get { return Subtools[(int) ToolMode.MaterialManager] as MaterialManager; } }
 
-    /// <summary>
-    /// Sets the selected asset on the active tool;
-    /// <br></br> The tool chooses whether the change is valid;
-    /// </summary>
-    /// <param name="path"></param>
-    public void SetSelectedAsset(string path) => Subtools[(int) toolMode].SetSelectedAsset(path);
+        /// <summary>
+        /// Sets the selected asset on the active tool;
+        /// <br></br> The tool chooses whether the change is valid;
+        /// </summary>
+        /// <param name="path"></param>
+        public void SetSelectedAsset(string path) => Subtools[(int) ActiveTool].SetSelectedAsset(path);
 
-    /// <summary>
-    /// Switch to the Prefabs Section of the Model Reader on the corresponding model;
-    /// </summary>
-    /// <param name="modelID"> ID of the model used to redirect the GUI; </param>
-    public void SwitchToLibrary(string modelID) {
-        MainGUI.SwitchActiveTool(ToolMode.ModelReader);
-        SetSelectedAsset(ModelAssetLibrary.ModelDataDict[modelID].path);
-        ModelReader.SetSelectedAssetMode(ModelAssetDatabaseReader.AssetMode.Model);
-        ModelReader.SetSelectedSection(ModelAssetDatabaseReader.SectionType.Prefabs);
-        GUIUtility.ExitGUI();
-    }
+        /// <summary>
+        /// Switch to the Prefabs Section of the Model Reader on the corresponding model;
+        /// </summary>
+        /// <param name="modelID"> ID of the model used to redirect the GUI; </param>
+        public void SwitchToLibrary(string modelID) {
+            MainGUI.SwitchActiveTool(ToolMode.ModelReader);
+            SetSelectedAsset(ModelAssetDatabase.ModelDataDict[modelID].path);
+            ModelReader.SetSelectedAssetMode(Reader.AssetMode.Model);
+            ModelReader.SetSelectedSection(Reader.SectionType.Prefabs);
+            GUIUtility.ExitGUI();
+        }
 
-    /// <summary>
-    /// Switch to the Prefab Organizer tool and place a Search String with prefab's name;
-    /// </summary>
-    /// <param name="prefabID"> ID of the prefab used to redirect the GUI; </param>
-    public void SwitchToOrganizer(string prefabID) {
-        MainGUI.SwitchActiveTool(ToolMode.PrefabOrganizer);
-        string modelID = ModelAssetLibrary.PrefabDataDict[prefabID].modelID;
-        string path = ModelAssetLibrary.ModelDataDict[modelID].path.RemovePathEnd("\\/");
-        string name = ModelAssetLibrary.PrefabDataDict[prefabID].name;
-        SetSelectedAsset(path);
-        PrefabOrganizer.SetSearchString(name);
-        GUIUtility.ExitGUI();
-    }
+        /// <summary>
+        /// Switch to the Prefab Organizer tool and place a Search String with prefab's name;
+        /// </summary>
+        /// <param name="prefabID"> ID of the prefab used to redirect the GUI; </param>
+        public void SwitchToOrganizer(string prefabID) {
+            MainGUI.SwitchActiveTool(ToolMode.PrefabOrganizer);
+            string modelID = ModelAssetDatabase.PrefabDataDict[prefabID].modelID;
+            string path = ModelAssetDatabase.ModelDataDict[modelID].path.RemovePathEnd("\\/");
+            string name = ModelAssetDatabase.PrefabDataDict[prefabID].name;
+            SetSelectedAsset(path);
+            PrefabOrganizer.SetSearchString(name);
+            GUIUtility.ExitGUI();
+        }
 
-    #endregion
+        #endregion
 
-    void OnEnable() {
-        ModelAssetLibraryConfigurationCore.LoadConfig();
-        ModelAssetLibrary.Refresh();
-        hierarchyBuilder = new ModelAssetLibraryHierarchyBuilder();
-        Subtools = new ModelAssetDatabaseTool[] { new ModelAssetDatabaseReader(),
-                                                  new ModelAssetDatabasePrefabOrganizer(),
-                                                  new ModelAssetDatabaseMaterialManager() };
-    }
+        void OnEnable() {
+            ConfigurationCore.LoadConfig();
+            ModelAssetDatabase.Refresh();
+            hierarchyBuilder = BaseTool.CreateTool<HierarchyBuilder>(this);
+            Subtools = new BaseTool[] { 
+                BaseTool.CreateTool<Reader>(this),
+                BaseTool.CreateTool<PrefabOrganizer>(this),
+                BaseTool.CreateTool<MaterialManager>(this),
+            };
+        }
 
-    void OnDisable() => FlushGlobalData();
+        void OnDisable() => FlushGlobalData();
 
-    void OnFocus() {
-        if (HasOpenInstances<ModelAssetDatabaseGUI>()
-            && MainGUI == null) MainGUI = GetWindow<ModelAssetDatabaseGUI>();
-    }
+        void OnFocus() {
+            if (HasOpenInstances<ModelAssetDatabaseGUI>()
+                && MainGUI == null) MainGUI = GetWindow<ModelAssetDatabaseGUI>();
+        }
 
-    void Update() {
-        if (highRepaintFrequency) Repaint();
-    }
+        void Update() {
+            if (highRepaintFrequency) Repaint();
+        }
 
-    void OnGUI() {
-        using (new EditorGUILayout.HorizontalScope()) {
-            using (new EditorGUILayout.VerticalScope(GUILayout.Width(200))) {
-                hierarchyBuilder.DrawSearchbar();
-                using (var leftScope = new EditorGUILayout.ScrollViewScope(directoryScroll,
-                                                                     false, true, GUI.skin.horizontalScrollbar,
-                                                                     GUI.skin.verticalScrollbar, UIStyles.PaddedScrollView)) {
-                    directoryScroll = leftScope.scrollPosition;
-                    hierarchyBuilder.ShowGUI(toolMode);
-                } DrawToolSelectionButtons();
-            } using (new GUILayout.VerticalScope()) {
-                using (new EditorGUILayout.HorizontalScope(EditorStyles.toolbar)) {
-                    DrawToolbarButtons();
-                } using (var rightScope = new EditorGUILayout.ScrollViewScope(toolScroll, UIStyles.MorePaddingScrollView)) {
-                    toolScroll = rightScope.scrollPosition;
-                    DrawActiveTool();
+        void OnGUI() {
+            using (new EditorGUILayout.HorizontalScope()) {
+                using (new EditorGUILayout.VerticalScope(GUILayout.Width(200))) {
+                    hierarchyBuilder.DrawToolbar();
+                    using (var leftScope = new EditorGUILayout.ScrollViewScope(directoryScroll,
+                                                                         false, true, GUI.skin.horizontalScrollbar,
+                                                                         GUI.skin.verticalScrollbar, UIStyles.PaddedScrollView)) {
+                        directoryScroll = leftScope.scrollPosition;
+                        hierarchyBuilder.ShowGUI();
+                    } DrawToolSelectionButtons();
+                } using (new GUILayout.VerticalScope()) {
+                    using (new EditorGUILayout.HorizontalScope(EditorStyles.toolbar)) {
+                        DrawToolbarButtons();
+                    } using (var rightScope = new EditorGUILayout.ScrollViewScope(toolScroll, UIStyles.MorePaddingScrollView)) {
+                        toolScroll = rightScope.scrollPosition;
+                        DrawActiveTool();
+                    }
                 }
             }
         }
-    }
 
-    /// <summary>
-    /// Draws buttons to switch from one tool to another below the hierarchy;
-    /// </summary>
-    private void DrawToolSelectionButtons() {
-        using (new EditorGUILayout.HorizontalScope(GUI.skin.box)) {
-            if (toolMode == ToolMode.ModelReader) { GUI.color = UIColors.Blue; GUI.enabled = false; }
-            if (GUILayout.Button(new GUIContent(EditorUtils.FetchIcon("d_PrefabModel Icon")),
-                                 EditorStyles.toolbarButton, GUILayout.MaxHeight(20))) {
-                SwitchActiveTool(ToolMode.ModelReader);
-            } if (toolMode == ToolMode.PrefabOrganizer) { GUI.color = UIColors.Blue; GUI.enabled = false; }
-            else { GUI.color = Color.white; GUI.enabled = true; }
-            if (GUILayout.Button(new GUIContent(EditorUtils.FetchIcon("d_PrefabVariant Icon")),
-                                   EditorStyles.toolbarButton, GUILayout.MaxHeight(20))) {
-                SwitchActiveTool(ToolMode.PrefabOrganizer);
-            } if (toolMode == ToolMode.MaterialManager) { GUI.color = UIColors.Blue; GUI.enabled = false; }
-            else { GUI.color = Color.white; GUI.enabled = true; }
-            if (GUILayout.Button(new GUIContent(EditorUtils.FetchIcon("d_Material Icon")),
-                                   EditorStyles.toolbarButton, GUILayout.MaxHeight(20))) {
-                SwitchActiveTool(ToolMode.MaterialManager);
-            } GUI.color = Color.white; GUI.enabled = true;
-        } 
-    }
-
-    /// <summary>
-    /// Switch to a different tool;
-    /// </summary>
-    public void SwitchActiveTool(ToolMode newToolMode) {
-        Subtools[(int) toolMode].ResetData();
-        Subtools[(int) newToolMode].RefreshData();
-        toolMode = newToolMode;
-    }
-
-    /// <summary> Draws the toolbar buttons depending on the currently selected tool; </summary>
-    private void DrawToolbarButtons() {
-        Subtools[(int) toolMode].DrawToolbar();
-        if (GUILayout.Button(EditorUtils.FetchIcon("_Popup"), EditorStyles.toolbarButton, GUILayout.MinWidth(32), GUILayout.MaxWidth(32))) {
-            ModelAssetLibraryConfigurationGUI.ShowWindow();
+        /// <summary>
+        /// Draws buttons to switch from one tool to another below the hierarchy;
+        /// </summary>
+        private void DrawToolSelectionButtons() {
+            using (new EditorGUILayout.HorizontalScope(GUI.skin.box)) {
+                if (ActiveTool == ToolMode.ModelReader) { GUI.color = UIColors.Blue; GUI.enabled = false; }
+                if (GUILayout.Button(new GUIContent(EditorUtils.FetchIcon("d_PrefabModel Icon")),
+                                     EditorStyles.toolbarButton, GUILayout.MaxHeight(20))) {
+                    SwitchActiveTool(ToolMode.ModelReader);
+                } if (ActiveTool == ToolMode.PrefabOrganizer) { GUI.color = UIColors.Blue; GUI.enabled = false; }
+                else { GUI.color = Color.white; GUI.enabled = true; }
+                if (GUILayout.Button(new GUIContent(EditorUtils.FetchIcon("d_PrefabVariant Icon")),
+                                       EditorStyles.toolbarButton, GUILayout.MaxHeight(20))) {
+                    SwitchActiveTool(ToolMode.PrefabOrganizer);
+                } if (ActiveTool == ToolMode.MaterialManager) { GUI.color = UIColors.Blue; GUI.enabled = false; }
+                else { GUI.color = Color.white; GUI.enabled = true; }
+                if (GUILayout.Button(new GUIContent(EditorUtils.FetchIcon("d_Material Icon")),
+                                       EditorStyles.toolbarButton, GUILayout.MaxHeight(20))) {
+                    SwitchActiveTool(ToolMode.MaterialManager);
+                } GUI.color = Color.white; GUI.enabled = true;
+            } 
         }
-    }
 
-    /// <summary> Draws the currently selected tool on the right side of the window; </summary>
-    private void DrawActiveTool() => Subtools[(int) toolMode].ShowGUI();
+        /// <summary>
+        /// Switch to a different tool;
+        /// </summary>
+        public void SwitchActiveTool(ToolMode newToolMode) {
+            Subtools[(int) ActiveTool].ResetData();
+            Subtools[(int) newToolMode].RefreshData();
+            ActiveTool = newToolMode;
+        }
 
-    /// <summary>
-    /// Unloads all data contained in the tool components;
-    /// </summary>
-    private void FlushGlobalData() {
-        foreach (ModelAssetDatabaseTool subtool in Subtools) subtool.FlushData();
-        //ModelAssetLibrary.UnloadDictionaries();
-        Resources.UnloadUnusedAssets();
-        SetHighRepaintFrequency(false);
-        toolMode = 0;
-    }
+        /// <summary> Draws the toolbar buttons depending on the currently selected tool; </summary>
+        private void DrawToolbarButtons() {
+            Subtools[(int) ActiveTool].DrawToolbar();
+            if (GUILayout.Button(EditorUtils.FetchIcon("_Popup"), EditorStyles.toolbarButton, GUILayout.MinWidth(32), GUILayout.MaxWidth(32))) {
+                ConfigurationGUI.ShowWindow();
+            }
+        }
 
-    public void SetHighRepaintFrequency(bool useHigh) {
-        highRepaintFrequency = useHigh;
+        /// <summary> Draws the currently selected tool on the right side of the window; </summary>
+        private void DrawActiveTool() => Subtools[(int) ActiveTool].ShowGUI();
+
+        /// <summary>
+        /// Unloads all data contained in the tool components;
+        /// </summary>
+        private void FlushGlobalData() {
+            //ModelAssetLibrary.UnloadDictionaries();
+            Resources.UnloadUnusedAssets();
+            SetHighRepaintFrequency(false);
+            ActiveTool = 0;
+        }
+
+        /// <summary>
+        /// Determines whether the Library should repaint on the Editor Update loop;
+        /// </summary>
+        /// <param name="useHigh"> True to repaint on Update, false otherwise; </param>
+        public void SetHighRepaintFrequency(bool useHigh) {
+            highRepaintFrequency = useHigh;
+        }
     }
 }
